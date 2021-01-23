1727a61bacdb0e73d5fe44e045761a72f4b23f8d
haileymm <haileymundt@gmail.com>
2021-01-22 17:39:27 -0800

    w03 changes

---
+++ lesson3/lesson3js/lesson3.js
@@ -0,0 +1,5 @@
+/*Footer year and last modified*/
+let year = new Date().getFullYear();
+let modified = document.lastModified;
+document.getElementById("year").innerHTML = "&#169; " + year + " Hailey Mundt | Nevada | ";
+document.getElementById("output").innerHTML = "Last updated: " + modified;
