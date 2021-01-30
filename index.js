c711957d8f270fc613fd623458605194af544b75
haileymm <haileymundt@gmail.com>
2021-01-30 15:21:24 -0800

    commit on Preston page

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ let year = new Date().getFullYear();
 let modified = document.lastModified;
 document.getElementById("year").innerHTML = "&#169; " + year + " Hailey Mundt | Nevada | ";
 document.getElementById("output").innerHTML = "Last updated: " + modified;
+
+
