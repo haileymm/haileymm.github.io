fc4945bbd4516d1087cce8cb2b4cbac2fe741a71
haileymm <haileymundt@gmail.com>
2021-02-18 18:11:16 -0800

    lazy load

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -10,7 +10,7 @@ const loadImages = (image) => {
 
 const imgOptions = {
     threshold: 1,
-    rootMargin: '0px 0px 200px 0px'
+    rootMargin: '0px 0px 100px 0px'
 };
 
 if ('IntersectionObserver' in window) {
