00a12a2341f4748afd83f1960de704f4fdd9abf9
haileymm <haileymundt@gmail.com>
2021-02-18 18:01:08 -0800

    lazy load

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,30 +1,52 @@
-// find anything with data-src attribute
-const images = document.querySelectorAll("[data-src]");
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if(!src) {
-        return;
-    }
-    img.src = src;
-}
 
-const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px 50px 0px"
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
 };
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
-        }
-    })
-}, imgOptions);
+const imgOptions = {
+    threshold: 0,
+    rootMargin: '0px 0px 200px 0px'
+};
 
-images.forEach(image => {
-    imgObserver.observe(image);
-});
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, imgObserver) => {
+        items.forEach(item => {
+            if (!item.isIntersecting) {
+                return;
+            }   else {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach(image => {
+        imgObserver.observe(image);
+    });
+}
+
+// //if supported, then ... if not supported just load all the images
+// if ('IntersectionObserver' in window) {
+//         const imgObserver = new IntersectionObserver((items) => {
+//             items.forEach(item) => {
+//                 if (item.isIntersecting) {
+//                     loadImages(item.target);
+//                     imgObserver.unobserve(item.target);
+//                 }
+//             });
+//         }, imgOptions);
+
+//         imagesToLoad.forEach(img) => {
+//             imgObserver.observe(img);
+//         });
+
+//         } else {
+//             imagesToLoad.forEach((img) => {
+//                 loadImages(img);
+//         });
+// }
